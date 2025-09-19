3f399789ade93dd4322c282ef437e2a61e8d7046
DevRehpotsirhc <christopher4ponte@gmail.com>
2025-09-19 09:46:52 -0500

    implement: se agrega el question-answering a los modelos nlp

--- nlp/views.py
+++ nlp/views.py
@@ -3,10 +3,20 @@ from transformers import pipeline
 
 clasificador = pipeline(
     "zero-shot-classification",
-    model="joeddav/xlm-roberta-large-xnli"
+    model="joeddav/xlm-roberta-large-xnli",
+    device="cuda"
 )
 
 def clasificar(texto: str, etiquetas: list):
     clasificacion = clasificador(texto, candidate_labels=etiquetas)
     return clasificacion
+
+respondedor = pipeline(
+    "question-answering",
+    model="deepset/xlm-roberta-base-squad2",
+    device="cuda"
+)
+
+def responder(pregunta: str, contexto: str):
+    return respondedor(pregunta, contexto)["answer"] # type:ignore
 
